6587970bc4c3668c52b7e941cc6c9e1c34b7dde2
Felipe Diniz <felipeard2006@gmail.com>
2023-01-09 21:23:44 -0300

    Adicionado a rota de de adicionar notas e CSS feito.

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const home = require('./src/controllers/home/home')
+const notes = require('./src/controllers/notes/notes')
 const route = express.Router();
 
+route.get("/notes", notes)
 route.get("/", home)
 
 module.exports = route;
