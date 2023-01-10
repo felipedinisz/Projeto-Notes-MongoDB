4e0cbf7f1990efd2bfc584ee8f01ba56d3fd796f
Felipe Diniz <felipeard2006@gmail.com>
2023-01-09 22:27:26 -0300

    Conexão com banco de dados bem-sucedida

--- app.js
+++ app.js
@@ -7,14 +7,25 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+//DB
+const db = require('./db/connection')
+
 //template engine
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs.engine());
 app.set("views", path.resolve(__dirname, "src", "views"));
-
 app.use(express.static(path.resolve(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Acessar http://localhost:${port}`);
-});
+db.initDb((err, db) => {
+  if(err) {
+    console.log(err)
+    return;
+  }
+
+  console.log("Database conectada com sucesso.")
+  app.listen(port, () => {
+    console.log(`Acessar http://localhost:${port}`)
+  })
+})
